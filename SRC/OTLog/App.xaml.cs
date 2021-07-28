88444229ab890c7bf1bb5ef7588b26fc774f5f10
Suence <Suence.Online@outlook.com>
2021-07-28 17:13:12 +0800

    开启自启动

--- SRC/OTLog/App.xaml.cs
+++ SRC/OTLog/App.xaml.cs
@@ -4,8 +4,11 @@ using OTLog.ViewModels;
 using OTLog.Views;
 using Prism.Ioc;
 using Prism.Modularity;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 
 namespace OTLog
@@ -37,6 +40,26 @@ namespace OTLog
 
             _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
             _notifyIcon.DataContext = Container.Resolve<NotifyIconViewModel>();
+
+            var systemStartupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            string linkFileFullPath = Path.Combine(systemStartupFolder, "OTLog.lnk");
+
+            // 如果快捷方式已经存在, 则不进行任何操作
+            if (File.Exists(linkFileFullPath)) return;
+
+            CreateShortcut(linkFileFullPath, "--nowindow");
+        }
+
+        // 创建快捷方式
+        private void CreateShortcut(string lnkFilePath, string args = "")
+        {
+            var shellType = Type.GetTypeFromProgID("WScript.Shell");
+            dynamic shell = Activator.CreateInstance(shellType);
+            var shortcut = shell.CreateShortcut(lnkFilePath);
+            shortcut.TargetPath = Assembly.GetEntryAssembly().Location;
+            shortcut.Arguments = args;
+            shortcut.WorkingDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            shortcut.Save();
         }
 
         protected override void OnExit(ExitEventArgs e)
