b07cf11b27be66100bf1dfc3938b08b0362558c3
Suence <Suence.Online@outlook.com>
2021-08-12 18:09:04 +0800

    单实例程序实现

--- SRC/OTLog/App.xaml.cs
+++ SRC/OTLog/App.xaml.cs
@@ -10,9 +10,11 @@ using Prism.Ioc;
 using Prism.Modularity;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Windows;
 using System.Windows.Media;
 
@@ -47,6 +49,7 @@ namespace OTLog
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            AppOnStartup();
             AppArgs = e.Args.ToList();
 
             AppFileHelper.ValidateApplicationFiles();
@@ -72,5 +75,64 @@ namespace OTLog
 
             moduleCatalog.AddModule<HomeModule>();
         }
+
+        #region Constants and Fields
+
+        /// <summary>The event mutex name.</summary>
+        private const string UniqueEventName = "6531c085-e1f3-4e6c-907e-63bb695d9dea";
+
+        /// <summary>The unique mutex name.</summary>
+        private const string UniqueMutexName = "a2ad1b11-6051-48ff-a8f4-8f86f5cea8f5";
+
+        /// <summary>The event wait handle.</summary>
+        private EventWaitHandle eventWaitHandle;
+
+        /// <summary>The mutex.</summary>
+        private Mutex mutex;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>The app on startup.</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        private void AppOnStartup()
+        {
+            bool isOwned;
+            this.mutex = new Mutex(true, UniqueMutexName, out isOwned);
+            this.eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, UniqueEventName);
+
+            // So, R# would not give a warning that this variable is not used.
+            GC.KeepAlive(this.mutex);
+
+            if (isOwned)
+            {
+                // Spawn a thread which will be waiting for our event
+                var thread = new Thread(
+                    () =>
+                    {
+                        while (this.eventWaitHandle.WaitOne())
+                        {
+                            Current.Dispatcher.BeginInvoke(
+                                (Action)(() => ((MainWindow)Current.MainWindow).BringToForeground()));
+                        }
+                    });
+
+                // It is important mark it as background otherwise it will prevent app from exiting.
+                thread.IsBackground = true;
+
+                thread.Start();
+                return;
+            }
+
+            // Notify other instance so it could bring itself to foreground.
+            this.eventWaitHandle.Set();
+
+            // Terminate this instance.
+            this.Shutdown();
+        }
+
+        #endregion
     }
 }
--- SRC/OTLog/Views/MainWindow.xaml.cs
+++ SRC/OTLog/Views/MainWindow.xaml.cs
@@ -18,6 +18,21 @@ namespace OTLog.Views
             InitializeComponent();
 
         }
+        /// <summary>Brings main window to foreground.</summary>
+        public void BringToForeground()
+        {
+            if (this.WindowState == WindowState.Minimized || this.Visibility == Visibility.Hidden)
+            {
+                this.Show();
+                this.WindowState = WindowState.Normal;
+            }
+
+            // According to some sources these steps gurantee that an app will be brought to foreground.
+            this.Activate();
+            this.Topmost = true;
+            this.Topmost = false;
+            this.Focus();
+        }
 
         public static void ShowToast()
         {
