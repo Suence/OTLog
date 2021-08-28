a16be3ce72c201fc7005bacfba46d8717d217071
Suence <Suence.Online@outlook.com>
2021-08-29 06:38:30 +0800

    增加切换主题时的过渡动画

---
+++ SRC/OTLog.Core/Events/ThemeChangingEvent.cs
@@ -0,0 +1,14 @@
+﻿using OTLog.Core.Enums;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTLog.Core.Events
+{
+    public class ThemeChangingEvent : PubSubEvent<Theme>
+    {
+    }
+}
